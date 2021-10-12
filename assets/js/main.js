a82d5bd7cdf20624419bff9aabf1ba0bed3a9546
lucamagenes <lucamgns33@gmail.com>
2021-10-12 21:09:26 +0200

    fixed bug correzione del tag aggiunta della variabile per la chiusura del menu aggiunta funzione

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,20 @@ console.log(dropdown);
 
 dropdown.addEventListener("click", myFunction);
 
+
+//aprire il menu
 function myFunction() {
-    var x = document.getElementsByClassName("hamburger-menu");
+    var x = document.querySelector(".hamburger-menu");
     if (x.style.display === "block") {
         x.style.display = "none";
     } else {
         x.style.display = "block";
     }
 }
+
+//chiudere il menu
+
+const dropdown_close = document.querySelector(".hamburger-menu > a");
+
+dropdown_close.addEventListener("click", myFunction);
 
